4c8beb3de180ee1f3493938890501ae3ba47e8d1
Michał Idzik <scorpmi@gmail.com>
2013-06-04 17:29:16 +0200

    Logging to file added to GoT example

--- src/main/java/pl/edu/agh/toik/cold/example/Spider.java
+++ src/main/java/pl/edu/agh/toik/cold/example/Spider.java
@@ -1,22 +1,47 @@
 package pl.edu.agh.toik.cold.example;
 
+import java.io.IOException;
+import java.lang.management.ManagementFactory;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.FileAppender;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PatternLayout;
+
 public class Spider extends SmallCouncilMember {
 
 	private StringBuilder littleBirdsReport = new StringBuilder();
 	private Spider otherSpider = null;
-	
-	public void spy(String who, String message) {
-		littleBirdsReport.append(who + " : " + message + "\n");
+
+	private static Logger log;
+
+	static {
+		try {
+			String logfileName = "GoT_"
+					+ ManagementFactory.getRuntimeMXBean().getName() + ".log";
+			BasicConfigurator.configure(new FileAppender(new PatternLayout(
+					"%d [%c{1}] %p - %m%n"), logfileName));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
+		log = Logger.getLogger(Spider.class);
+	}
+
+	synchronized public void spy(String who, String message) {
+		String note = who + " : " + message;
+		littleBirdsReport.append(note + "\n");
+
 		if (otherSpider != null) {
 			otherSpider.spy(who, message);
 		}
+		log.info(note);
 	}
-	
+
 	public void printLittleBirdsReport() {
 		System.out.println(littleBirdsReport.toString());
 	}
-	
+
 	@Override
 	public void vote(String query) {
 	}
@@ -28,7 +53,5 @@ public class Spider extends SmallCouncilMember {
 	public void setOtherSpider(Spider otherSpider) {
 		this.otherSpider = otherSpider;
 	}
-	
+
 }
-
-
